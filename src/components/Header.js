b80a999c19597fc6453c96dd39d1cba2cd9c2e9f
afozbek <furkanozbek1995@gmail.com>
2020-06-27 13:59:03 +0300

    sidebar finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 
 const Header = () => {
+  const [isSideMenuOpened, setSideMenuOpened] = useState(false);
+
+  const sideMenuRef = useRef();
+
+  const openSideMenu = () => {
+    setSideMenuOpened(true);
+  };
+
+  const overlaySideMenuClickHandler = (e) => {
+    if (e.target === sideMenuRef.current) {
+      setSideMenuOpened(false);
+    }
+  };
+
   return (
     <header className="o-header">
       <img
@@ -10,23 +24,38 @@ const Header = () => {
         height="50"
       />
 
-      <div className="o-header__desc">
+      <div className={`o-header__desc ${isSideMenuOpened ? '-hide' : ''}`}>
         <button className="o-header__descBtn">Explore Tournaments</button>
         <button className="o-header__descBtn">New User Guide</button>
       </div>
 
-      <div className="o-header__auth">
+      <div className={`o-header__auth ${isSideMenuOpened ? '-hide' : ''}`}>
         <button className="o-header__authBtn">Sign Up</button>
         <button className="o-header__authBtn">Sign In</button>
       </div>
 
-      <input
-        id="hamburgerCheck"
-        name="hamburgerCheck"
-        style={{ display: 'none' }}
-        type="checkbox"
-      />
-      <label for="hamburgerCheck" className="o-header__hamburger"></label>
+      <div className="o-header__hamburgerWrapper" onClick={openSideMenu}>
+        <label className="o-header__hamburger" onClick={openSideMenu}></label>
+      </div>
+
+      <div
+        ref={sideMenuRef}
+        onClick={overlaySideMenuClickHandler}
+        className={`o-header__sideMenu ${isSideMenuOpened ? '-open' : ''}`}
+      >
+        <div className="o-header__sideMenuContent">
+          <button className="o-header__descBtn -side">
+            Explore Tournaments
+          </button>
+          <hr />
+          <button className="o-header__descBtn -side">New User Guide</button>
+          <hr />
+          <button className="o-header__authBtn -side">Sign Up</button>
+          <hr />
+          <button className="o-header__authBtn -side">Sign In</button>
+          <hr />
+        </div>
+      </div>
     </header>
   );
 };
